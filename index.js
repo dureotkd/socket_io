d90ed9a788d5a85def7c44a64904034941d9dce3
duerotkd <dureotkd123@naver.com>
2021-09-15 00:00:07 +0900

    zz

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+let room = ["room0", "room1", "room2"];
+let a = 0;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  console.log("유저가 들어왔다.");
+
+  // 요거 추가
+  socket.on("joinRoom", (num, name) => {
+    console.log(num, name);
+    socket.join(room[num], () => {
+      io.to(room[num]).emit("joinRoom", num, name);
+    });
+  });
+
+  // 요거 추가
+  socket.on("leaveRoom", (num, name) => {
+    console.log("현재 접속방 LEAVE ROOM ==>" + room[num]);
+    socket.leave(room[num], () => {
+      io.to(room[num]).emit("leaveRoom", num, name);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("유저가 나갔다.");
+  });
+
+  socket.on("chat-msg", (num, name, msg) => {
+    a = num;
+    console.log("현재 접속방 MSG SEND ==>" + room[a]);
+    io.to(room[a]).emit("chat-msg", name, msg); // to(room[a])를 통해 그룹에게만 메세지를 날린다.
   });
 });
 
